55573c2204726a321209390a205f6df5f86836f1
Nathaniel091 <nathanielsamuel091@gmail.com>
2020-09-10 16:24:18 +0100

    added the 'DOMContentLoaded' eventListener

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementsByTagName("body")[0];
 
-// when you select a value for the input, the 'setGradient' function will be called
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
 
-// lets write the setGradient function
+// This function is reponsible for displaying the value of color1 and color2 (i.e linear-gradient [css code]) in text-format inside the 'h3' element
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 	h3.textContent = body.style.background + ";";
 }
+
+// this allow the h3 element to have a preset value when the page is loaded instead of displaying none
+document.addEventListener('DOMContentLoaded', ()=>{
+	setGradient()
+})
+
+// when you select a value for the input, the 'setGradient' function will be called
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
